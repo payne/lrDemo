809a47a7d947c32b0d3e7ee05275db978b9fa5f5
Rolf <rroessing@googlemail.com>
2013-07-08 16:11:33 +0200

    'initialized'

---
+++ src/main/java/me/roessing/lrdemo/HelloController.java
@@ -0,0 +1,18 @@
+package me.roessing.lrdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.Date;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello world!");
+        model.addAttribute("dateTime", new Date());
+		return "hello";
+	}
+}
